b463850a05b8308d2d0a53f488c69d13feed297d
Shirshak Sharma <shirshak.sharma@gmail.com>
2020-12-03 22:20:14 -0500

    day 1: report repair

---
+++ code/one/one.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+
+const entries = {};
+
+fs.readFile("code/one/input.txt", "utf-8", (err, data) => {
+  if (data) {
+    data.split("\n").map((entry) => {
+      entries[entry] = 2020 - entry;
+    });
+  }
+
+  // Part 1
+  Object.keys(entries).forEach((entry) => {
+    if (entries[entries[entry]]) {
+      console.log("Part 1 : Found it");
+      console.log(entries[entry] * entries[entries[entry]]);
+    }
+  });
+
+  // Part 2
+  Object.keys(entries).forEach((entry1) => {
+    Object.keys(entries).forEach((entry2) => {
+      const remaining = 2020 - entry1 - entry2;
+      if (remaining > 0 && entries[remaining]) {
+        console.log("Found it");
+        // entry1 and entry2 are strings. Don't add them before converting...
+        console.log(remaining * Number(entry1) * Number(entry2));
+      }
+    });
+  });
+});
